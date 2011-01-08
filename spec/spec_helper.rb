af56bc4c6078774c7fb381eb86db62342c9db156
Dan Ryan <hi@iamdanryan.com>
2011-01-08 13:10:11 -0500

    fix authentication

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'spice'
 
+require 'rest-client'
+require 'mixlib-authentication'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
