90b39e836dd23a2a9e2cf1eeb8d0139bfba60665
Sean Porter <portertech@gmail.com>
2011-03-27 12:46:01 -0700

    hacking around w/ search

--- lib/spice/search.rb
+++ lib/spice/search.rb
@@ -1,17 +1,17 @@
 module Spice
   class Search < Spice::Chef
-    INDEXES = %w(node role client users)
-    
-    INDEXES.each do |index|
-      define_method index do |options|
-        options ||= {}
-        
-        query = options[:q] || '*:*'
-        sort = options[:sort] || "X_CHEF_id_CHEF_X asc"
-        start = options[:start] || 0
-        rows = options[:rows] || 1000
-        
-        connection.get("/search/#{index}?q=#{query}&sort=#{sort}&start=#{start}&rows=#{rows}")
+    class << self
+      %w{node role client users}.each do |index|
+        define_method index do |options|
+          options ||= {}
+
+          query = options[:q] || '*:*'
+          sort = options[:sort] || "X_CHEF_id_CHEF_X asc"
+          start = options[:start] || 0
+          rows = options[:rows] || 1000
+ 
+          connection.get("/search/#{index}?q=#{query}&sort=#{sort}&start=#{start}&rows=#{rows}")
+        end
       end
     end
   end
